6839a757f48d9ff8302ae9d70846d414200a123b
Jo Dickson <jogreybill@gmail.com>
2020-06-15 10:56:16 -0400

    Updated version number

--- gmucf-utilities.php
+++ gmucf-utilities.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: GMUCF Utilities
 Description: Feature and utility plugin for the GMUCF WordPress site.
-Version: 0.0.0
+Version: 1.0.0
 Author: UCF Web Communications
 License: GPL3
 GitHub Plugin URI: UCF/GMUCF-Utilities
